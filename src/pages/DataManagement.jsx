ceedeeefe9b9fcdfb72a23a2310d076ff9e96d2d
333cnrbl6 <333cnrbl6@gmail.com>
2026-02-05 22:44:13 +0000

    File changes

--- src/pages/DataManagement.jsx
+++ src/pages/DataManagement.jsx
@@ -17,6 +17,7 @@ import CompareSpecies from '@/components/species/CompareSpecies';
 import SpeciesListManager from '@/components/species/SpeciesListManager';
 import SpeciesNotes from '@/components/species/SpeciesNotes';
 import DataMetricsModal from '@/components/DataMetricsModal';
+import MergeCandidatesModal from '@/components/MergeCandidatesModal';
 import OnboardingWizard from '@/components/OnboardingWizard';
 import BangOnLogo from '@/components/BangOnLogo';
 import LogoShowcase from '@/components/LogoShowcase';
